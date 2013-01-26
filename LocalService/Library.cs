e44c4bd0341b2fb16e6ca10b816a01d39b38366b
Ferdinand Prantl <prantlf@gmail.com>
2013-01-26 12:55:45 +0100

    ~ Library interface modified to be usable for SharePoint BCS

--- LocalService/Library.cs
+++ LocalService/Library.cs
@@ -16,92 +16,225 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 
 namespace Gutenberg.LocalService
 {
-    [ServiceContract]
+    [ServiceContract, XmlSerializerFormat]
     public interface Library
     {
         [OperationContract]
-        Book GetBook(string name);
+        Book GetBook(int number);
 
         [OperationContract]
-        Book[] GetBooks(int start, int count);
+        Book[] GetBooks();
+
+        [OperationContract]
+        Stream ReadContent(int number);
+
+        [OperationContract]
+        Volume ReadVolume(int number);
+
+        [OperationContract]
+        Card[] EnumerateCards(int lastNumberSeen);
+
+        //[OperationContract]
+        //Book[] FindBooks(Filter filter, int start, int count);
+    }
+
+    [DataContract]
+    public class Card
+    {
+        [DataMember(IsRequired = true)]
+        public int Number;
+
+        [DataMember(IsRequired = true)]
+        public DateTime LastModified;
     }
 
     [DataContract]
     public class Book
     {
-        [DataMember]
-        public int Number { get; set; }
+        [DataMember(IsRequired = true)]
+        public int Number;
 
-        [DataMember]
-        public string Title { get; set; }
+        [DataMember(IsRequired = true)]
+        public DateTime LastModified;
 
-        [DataMember]
-        public string[] Authors { get; set; }
+        [DataMember(IsRequired = true)]
+        public string Title;
 
-        [DataMember]
-        public string[] Contributors { get; set; }
+        [DataMember(IsRequired = true)]
+        public string Author { get; set; }
 
-        [DataMember]
-        public YearSpan Era { get; set; }
+        [DataMember(IsRequired = true)]
+        public string Contributor { get; set; }
 
-        [DataMember]
+        [DataMember(IsRequired = true)]
+        public string Notes { get; set; }
+
+        [DataMember(IsRequired = true)]
+        public string Tags { get; set; }
+
+        [DataMember(IsRequired = true)]
         public string Language { get; set; }
 
-        [DataMember]
-        public string[] Notes { get; set; }
+        [DataMember(IsRequired = true)]
+        public int Size;
 
-        [DataMember]
-        public string[] Tags { get; set; }
+        [DataMember(IsRequired = true)]
+        public string Era;
 
-        [DataMember]
-        public Date Included { get; set; }
+        [DataMember(IsRequired = true)]
+        public DateTime Included;
 
-        [DataMember]
-        public int Downloads { get; set; }
+        [DataMember(IsRequired = true)]
+        public int Downloads;
 
-        [DataMember]
-        public Volume[] Volumes { get; set; }
+        [DataMember(IsRequired = true)]
+        public string FileName;
 
-        internal static Book Create(Gutenberg.Book book) {
-            return new Book {
-                Number = book.Number, Title = book.Title, Authors = book.Authors,
-                Contributors = book.Contributors, Era = book.Era, Language = book.Language,
-                Notes = book.Notes, Tags = book.Tags, Included = book.Included,
-                Downloads = book.Downloads, Volumes = book.Volumes == null ? null :
-                    book.Volumes.Select(volume => Volume.Create(volume)).ToArray()
-            };
-        }
+        [DataMember(IsRequired = true)]
+        public string MimeType;
     }
 
     [DataContract]
     public class Volume
     {
-        [DataMember]
-        public int Number { get; set; }
+        [DataMember(IsRequired = true)]
+        public string FileName;
 
-        [DataMember]
-        public string URL { get; set; }
+        [DataMember(IsRequired = true)]
+        public string MimeType;
 
-        [DataMember]
-        public string[] Formats { get; set; }
+        [DataMember(IsRequired = true)]
+        public byte[] Content;
+    }
 
-        [DataMember]
-        public Date Uploaded { get; set; }
+    static class Extensions
+    {
+        public const string MimeType = "text/plain";
 
-        [DataMember]
-        public int Size { get; set; }
+        public static Card ToCard(this Gutenberg.Book book) {
+            return new Card {
+                Number = book.Number,
+                LastModified = new DateTime(book.GetVolume(MimeType).Uploaded.Ticks)
+            };
+        }
 
-        internal static Volume Create(Gutenberg.Volume volume) {
+        public static Book ToBook(this Gutenberg.Book book) {
+            var volume = book.GetVolume(MimeType);
+            return new Book {
+                Number = book.Number, Title = book.Title, MimeType = MimeType,
+                Language = book.Language, LastModified = new DateTime(volume.Uploaded.Ticks),
+                Size = volume.Size, FileName = book.FriendlyTitle + ".txt",
+                Author = book.Authors == null ? null : string.Join(", ", book.Authors),
+                Contributor = book.Contributors == null ? null :
+                                    string.Join(", ", book.Contributors),
+                Notes = book.Notes == null ? null : string.Join("\r\n", book.Notes),
+                Tags = book.Tags == null ? null : string.Join("\r\n", book.Tags),
+                Downloads = book.Downloads, Included = new DateTime(book.Included.Ticks),
+                Era = book.Era.ToString()
+            };
+        }
+
+        public static Volume ToVolume(this Gutenberg.Book book, byte[] content) {
             return new Volume {
-                Number = volume.Number, URL = volume.URL, Formats = volume.Formats,
-                Uploaded = volume.Uploaded, Size = volume.Size
+                MimeType = MimeType, FileName = book.FriendlyTitle + ".txt", Content = content
             };
         }
     }
+
+    //[DataContract]
+    //public class Filter
+    //{
+    //    [DataMember(IsRequired = true)]
+    //    public string Title { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string Author { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string Language { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string Format { get; set; }
+    //}
+
+    //[DataContract]
+    //public class Book
+    //{
+    //    [DataMember(IsRequired = true)]
+    //    public int Number { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string Title { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string[] Authors { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string[] Contributors { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public YearSpan Era { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string Language { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string[] Notes { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string[] Tags { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public Date Included { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public int Downloads { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public Volume[] Volumes { get; set; }
+
+    //    internal static Book Create(Gutenberg.Book book) {
+    //        return new Book {
+    //            Number = book.Number, Title = book.Title, Authors = book.Authors,
+    //            Contributors = book.Contributors, Era = book.Era, Language = book.Language,
+    //            Notes = book.Notes, Tags = book.Tags, Included = book.Included,
+    //            Downloads = book.Downloads, Volumes = book.Volumes == null ? null :
+    //                book.Volumes.Select(volume => Volume.Create(volume)).ToArray()
+    //        };
+    //    }
+    //}
+
+    //[DataContract]
+    //public class Volume
+    //{
+    //    [DataMember(IsRequired = true)]
+    //    public int Number { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string URL { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public string[] Formats { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public Date Uploaded { get; set; }
+
+    //    [DataMember(IsRequired = true)]
+    //    public int Size { get; set; }
+
+    //    internal static Volume Create(Gutenberg.Volume volume) {
+    //        return new Volume {
+    //            Number = volume.Number, URL = volume.URL, Formats = volume.Formats,
+    //            Uploaded = volume.Uploaded, Size = volume.Size
+    //        };
+    //    }
+    //}
 }
--- LocalService/LibraryService.svc.cs
+++ LocalService/LibraryService.svc.cs
@@ -16,9 +16,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.ServiceModel.Activation;
+using System.Text;
 using Gutenberg.FileSystem;
 
 namespace Gutenberg.LocalService
@@ -26,32 +29,94 @@ namespace Gutenberg.LocalService
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class LibraryService : Library
     {
-        public Book GetBook(string name) {
-            if (name == null)
-                throw new ArgumentNullException("name");
-            if (name.IsEmpty())
-                throw new ArgumentException("Book name must not be empty.", "name");
-            return Book.Create(Cache.GetBook(name));
+        public Book GetBook(int number) {
+            Log.Verbose("Getting the book {0}.", number);
+            return OpenBook(number).ToBook();
         }
 
-        public Book[] GetBooks(int start, int count) {
-            if (start < 0 || start > Cache.Books.Count())
-                throw new ArgumentOutOfRangeException("start");
-            if (count < 0)
-                throw new ArgumentOutOfRangeException("count");
-            return Cache.Books.Skip(start).Take(count).Select(book =>
-                                                            Book.Create(book)).ToArray();
+        public Book[] GetBooks() {
+            Log.Verbose("Getting the first 100 books.");
+            return Books.Take(100).Select(book => book.ToBook()).ToArray();
         }
 
-        Cache Cache {
+        public Stream ReadContent(int number) {
+            Log.Verbose("Reading the book {0} content.", number);
+            Gutenberg.Book book;
+            return OpenContent(number, out book);
+        }
+
+        public Volume ReadVolume(int number) {
+            Log.Verbose("Reading the book {0} volume.", number);
+            Gutenberg.Book book;
+            using (var stream = OpenContent(number, out book))
+                return book.ToVolume(stream.ReadBytes());
+        }
+
+        public Card[] EnumerateCards(int lastNumberSeen) {
+            Log.Verbose("Enumerating books starting after {0}.", lastNumberSeen);
+            var books = Books;
+            if (lastNumberSeen != 0)
+                books = books.SkipWhile(book => lastNumberSeen != book.Number).Skip(1);
+            return books.Take(100).Select(book => book.ToCard()).ToArray();
+        }
+
+        Gutenberg.Book OpenBook(int number) {
+            return Cache.GetBook(number.ToString(CultureInfo.InvariantCulture));
+        }
+
+        Stream OpenContent(int number, out Gutenberg.Book book) {
+            book = OpenBook(number);
+            var volume = book.GetVolume(Extensions.MimeType);
+            var files = new FileSource { Log = Log };
+            Encoding encoding = null;
+            return files.Open(volume, ref encoding);
+        }
+
+        //public Book[] FindBooks(Filter filter, int start, int count) {
+        //    if (start < 0 || start > Cache.Books.Count())
+        //        throw new ArgumentOutOfRangeException("start");
+        //    if (count < 0)
+        //        throw new ArgumentOutOfRangeException("count");
+        //    var books = Cache.Books;
+        //    if (filter != null) {
+        //        if (!string.IsNullOrEmpty(filter.Title))
+        //            books = books.Where(book => !string.IsNullOrEmpty(book.Title) &&
+        //                            book.Title.LikeCI(filter.Title));
+        //        if (!string.IsNullOrEmpty(filter.Author))
+        //            books = books.Where(book => book.Authors != null &&
+        //                book.Authors.Any(author => author.LikeCI(filter.Author)));
+        //        if (!string.IsNullOrEmpty(filter.Language))
+        //            books = books.Where(book => !string.IsNullOrEmpty(book.Language) &&
+        //                            book.Language.LikeII(filter.Language));
+        //        if (!string.IsNullOrEmpty(filter.Format))
+        //            books = books.Where(book => book.Formats != null &&
+        //                book.Formats.Any(author => author.LikeII(filter.Format)));
+        //    }
+        //    return books.Skip(start).Take(count).Select(book => Book.Create(book)).ToArray();
+        //}
+
+        IEnumerable<Gutenberg.Book> Books {
+            get {
+                return Cache.Books.Where(book => book.Formats != null &&
+                    book.Formats.Any(format => format.StartsWithII(Extensions.MimeType)));
+            }
+        }
+
+        static Cache Cache {
             get { return cache ?? (cache = CreateCache()); }
         }
         static Cache cache;
 
         internal static Cache CreateCache() {
             return new Cache() {
-                BookSource = new BookSource(), VolumeSource = new VolumeSource()
+                BookSource = new BookSource { Log = Log },
+                VolumeSource = new VolumeSource { Log = Log }, Log = Log
             };
         }
+
+        static Log Log {
+            get { return log ?? (log = new DebugLog()); }
+        }
+        static Log log;
     }
 }
