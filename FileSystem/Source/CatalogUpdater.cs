1dc507e88917cb47c9c621d9e8621fe404cf6982
Ferdinand Prantl <prantlf@gmail.com>
2013-01-27 19:47:35 +0100

    ~ logic of updating the local catalog moved from PowerShell to FileSystem

---
+++ FileSystem/Source/CatalogUpdater.cs
@@ -0,0 +1,117 @@
+﻿// Copyright (C) 2012-2013 Ferdinand Prantl <prantlf@gmail.com>
+// All rights reserved.       
+//
+// This file is part of the Project Gutenberg Access from Local File System
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.IO;
+using System.Xml;
+
+namespace Gutenberg.FileSystem
+{
+    public class CatalogUpdater : LoggableBase
+    {
+        public bool ShouldUpdate(BookSource books, VolumeSource volumes, out Date date) {
+            var progress = Log.Action(3, "Checking local catalog...");
+            progress.Continue("Opening local catalog...");
+            if (books.Exists && volumes.Exists) {
+                progress.Continue("Getting local creation date...");
+                date = Date.Min(books.GetCreated(), volumes.GetCreated());
+                Log.Verbose("Local creation date: {0}.", date);
+                if (new Date(DateTime.Now) == date) {
+                    Log.Warning("Local catalog up-to-date.");
+                    progress.Finish();
+                    return false;
+                }
+            } else {
+                date = Date.MinValue;
+            }
+            progress.Finish();
+            return true;
+        }
+
+        public bool TryUpdate(Stream catalogStream, BookSource books, VolumeSource volumes,
+                              Func<Date, bool> update, Func<bool> confirm) {
+            var progress = Log.Action(5, "Trying catalog update...");
+            progress.Continue("Opening remote catalog...");
+            var parser = new CatalogParser { Log = Log };
+            using (var reader = parser.Open(catalogStream)) {
+                progress.Continue("Checking remote creation date...");
+                var date = parser.GetCreated(reader);
+                Log.Verbose("Remote creation date: {0}.", date);
+                if (update(date)) {
+                    progress.Continue("Updating local catalog...");
+                    string bookPath = null, volumePath = null;
+                    try {
+                        using (var bookStream = books.Create(out bookPath))
+                        using (var volumeStream = volumes.Create(out volumePath))
+                            Convert(parser, reader, date, bookStream, volumeStream);
+                        progress.Continue("Copying converted catalog...");
+                        if (confirm()) {
+                            books.Update(bookPath);
+                            volumes.Update(volumePath);
+                        }
+                    } finally {
+                        if (bookPath != null)
+                            IOUtility.DeleteTempFile(bookPath);
+                        if (volumePath != null)
+                            IOUtility.DeleteTempFile(volumePath);
+                    }
+                    Log.Verbose("Local catalog updated.");
+                    progress.Finish();
+                    return true;
+                }
+                Log.Warning("Local catalog up-to-date.");
+                progress.Finish();
+                return false;
+            }
+        }
+
+        public void Convert(CatalogParser parser, XmlReader reader, Date created,
+                            Stream bookStream, Stream volumeStream) {
+            var progress = Log.Action(55, "Converting catalog...");
+            int bookCount = 0, skippedCount = 0, volumeCount = 0, totalCount = 0;
+            var bookParser = new BookParser { Log = Log };
+            var volumeParser = new VolumeParser { Log = Log };
+            using (var bookWriter = bookParser.Create(bookStream, created))
+            using (var volumeWriter = volumeParser.Create(volumeStream, created)) {
+                foreach (var item in parser.GetItems(reader)) {
+                    var book = item as Book;
+                    if (book != null) {
+                        // Book 9140 and a couple of others are invalid.
+                        if (string.IsNullOrEmpty(book.Title)) {
+                            Log.Verbose("Skipping book {0} with no title.", book.Number);
+                            ++skippedCount;
+                        } else {
+                            bookParser.Write(bookWriter, book);
+                        }
+                        ++bookCount;
+                    } else {
+                        volumeParser.Write(volumeWriter, (Volume) item);
+                        ++volumeCount;
+                    }
+                    if (++totalCount % 10000 == 0)
+                        progress.Continue("{0} items processed...", totalCount);
+                }
+                bookParser.Finalize(bookWriter);
+                volumeParser.Finalize(volumeWriter);
+            }
+            Log.Verbose("{0} books and {1} volumes were processed, {2} books skipped.",
+                bookCount, volumeCount, skippedCount);
+            progress.Finish();
+        }
+    }
+}
--- PowerShell/Command/UpdateCatalog.cs
+++ PowerShell/Command/UpdateCatalog.cs
@@ -17,9 +17,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
-using System.IO;
 using System.Management.Automation;
-using System.Xml;
 using Gutenberg.FileSystem;
 
 namespace Gutenberg.PowerShell
@@ -46,112 +44,28 @@ namespace Gutenberg.PowerShell
         }
 
         void CheckOrUpdateCatalog() {
-            var progress = Log.Action(5, "Catalog Update");
+            var progress = Log.Action(2, "Checking and/or updating catalog...");
             var books = new BookSource { Log = Log };
             var volumes = new VolumeSource { Log = Log };
             if (!string.IsNullOrEmpty(ActualDirectory)) {
                 books.SetDirectory(ActualDirectory);
                 volumes.SetDirectory(ActualDirectory);
             }
+            var updater = new CatalogUpdater { Log = Log };
             Date localDate = Date.MinValue;
-            if (!Always && books.Exists && volumes.Exists) {
-                progress.Continue("Opening local catalog...");
-                localDate = books.GetCreated();
-                Log.Verbose("Local creation date: {0}.", localDate);
-                if (new Date(DateTime.Now) == localDate) {
-                    WriteWarning("Local catalog up-to-date.");
-                    progress.Finish();
-                    return;
-                }
+            if (Always || updater.ShouldUpdate(books, volumes, out localDate)) {
+                progress.Continue("Downloading remote catalog...");
+                var catalog = new Catalog { Log = Log };
+                if (!string.IsNullOrEmpty(URL))
+                    catalog.SetUrl(URL);
+                using (var stream = catalog.Open())
+                    updater.TryUpdate(stream, books, volumes,
+                        remoteDate => localDate == Date.MinValue || localDate < remoteDate,
+                        () => ShouldProcess("Catalog", "Update") && (Force ||
+                        ShouldContinue("Do you really want to update the local catalog?",
+                                       "Catalog Update")));
             }
-            progress.Continue("Downloading remote catalog...");
-            var catalog = new Catalog { Log = Log };
-            if (!string.IsNullOrEmpty(URL))
-                catalog.SetUrl(URL);
-            using (var stream = catalog.Open()) {
-                progress.Continue("Opening remote catalog...");
-                var parser = new CatalogParser { Log = Log };
-                using (var reader = parser.Open(stream)) {
-                    progress.Continue("Getting remote creation date...");
-                    var remoteDate = parser.GetCreated(reader);
-                    Log.Verbose("Remote creation date: {0}.", remoteDate);
-                    if (localDate == Date.MinValue || localDate < remoteDate) {
-                        progress.Continue("Updating local catalog...");
-                        UpdateLibrary(parser, reader, remoteDate, books, volumes);
-                        Log.Verbose(localDate == Date.MinValue ?
-                            "Local catalog created." : "Local catalog updated.");
-                    } else {
-                        WriteWarning("Local catalog up-to-date.");
-                    }
-                }
-                progress.Finish();
-            }
-        }
-
-        void UpdateLibrary(CatalogParser parser, XmlReader reader, Date created,
-                           BookSource books, VolumeSource volumes) {
-            var progress = Log.Action(56, "Catalog Conversion");
-            string bookPath = null, volumePath = null;
-            Stream bookStream = null, volumeStream = null;
-            BinaryWriter bookWriter = null, volumeWriter = null;
-            try {
-                int bookCount = 0, skippedCount = 0, volumeCount = 0, totalCount = 0;
-                bookStream = books.Create(out bookPath);
-                var bookParser = new BookParser { Log = Log };
-                bookWriter = bookParser.Create(bookStream, created);
-                volumeStream = volumes.Create(out volumePath);
-                var volumeParser = new VolumeParser { Log = Log };
-                volumeWriter = volumeParser.Create(volumeStream, created);
-                foreach (var item in parser.GetItems(reader)) {
-                    var book = item as Book;
-                    if (book != null) {
-                        // Book 9140 and a couple of others are invalid.
-                        if (string.IsNullOrEmpty(book.Title)) {
-                            Log.Verbose("Skipping book {0} with no title.", book.Number);
-                            ++skippedCount;
-                        } else {
-                            bookParser.Write(bookWriter, book);
-                        }
-                        ++bookCount;
-                    } else {
-                        volumeParser.Write(volumeWriter, (Volume) item);
-                        ++volumeCount;
-                    }
-                    if (++totalCount % 10000 == 0)
-                        progress.Continue("{0} items processed...", totalCount);
-                }
-                bookParser.Finalize(bookWriter);
-                volumeParser.Finalize(volumeWriter);
-                bookWriter.Close();
-                volumeWriter.Close();
-                Log.Verbose("{0} books and {1} volumes were processed, {2} books skipped.",
-                    bookCount, volumeCount, skippedCount);
-                progress.Continue("Copying converted catalog...", totalCount);
-                if (ShouldProcess("Catalog", "Update") && (Force ||
-                    ShouldContinue("Do you really want to update the local cataogue?",
-                                   "Catalog Update"))) {
-                    books.Update(bookPath);
-                    volumes.Update(volumePath);
-                } else {
-                    IOUtility.DeleteTempFile(bookPath);
-                    IOUtility.DeleteTempFile(volumePath);
-                }
-                progress.Finish();
-            } catch {
-                if (bookWriter != null)
-                    bookWriter.Close();
-                if (bookStream != null)
-                    bookStream.Close();
-                if (volumeWriter != null)
-                    volumeWriter.Close();
-                if (volumeStream != null)
-                    volumeStream.Close();
-                if (bookPath != null)
-                    IOUtility.DeleteTempFile(bookPath);
-                if (volumePath != null)
-                    IOUtility.DeleteTempFile(volumePath);
-                throw;
-            }
+            progress.Finish();
         }
     }
 }
